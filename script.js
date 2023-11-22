fe36dcda827f576a08667af50509826a9a6b27cb
Poescal08 <150332414+Poescal08@users.noreply.github.com>
2023-11-22 15:27:53 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function loadContent(section) {
+    var contentDiv = document.getElementById('content');
+
+    if (section === 'home') {
+      contentDiv.innerHTML = 
+        <div id="home">
+          <h1>Welkom op de Homepagina</h1>
+          <p>Dit is de inhoud van de homepagina.Voeg hier je eigen tekst toe.</p>
+        </div>
+      ;
+
+        } else if (section === 'Aboutme') {
+      contentDiv.innerHTML = 
+        <div id="Aboutme">
+          <h1>About Me</h1>
+          <article> Dit is de inhoud van de sectie over het Aboutme. Hallo mijn naam is Maarten en leuk dat je op mijn website kijkt. Momenteel zit ik op school bij het clv. Mijn hobbies zijn gamen en chillen met vrienden.</article>
+        </div>
+      ;
+
+    } else if (section === 'binairstelsel') {
+      contentDiv.innerHTML = 
+        <div id="binairstelsel">
+          <h1>Binairstelsel</h1>
+          <p>Dit is de inhoud van de sectie over het binairstelsel. Voeg hier je eigen tekst toe.</p>
+        </div>
+      ;
+    } else if (section === 'kleurmodellen') {
+      contentDiv.innerHTML = 
+        <div id="kleurmodellen">
+          <h1>Kleurmodellen</h1>
+          <p>Dit is de inhoud van de sectie over kleurmodellen. Voeg hier je eigen tekst toe.</p>
+        </div>
+      ;
+
+    }
+  }
